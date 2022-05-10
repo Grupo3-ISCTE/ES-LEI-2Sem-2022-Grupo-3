7b566ccb51ef2d80aa7535dc725fcbbd329a3681
bphbs-iscte <bphbs@iscte-iul.pt>
2022-05-10 18:40:15 +0100

    Remoção de deadcode

--- src/test/java/org/jfree/chart/renderer/xy/XYBarRendererTest.java
+++ src/test/java/org/jfree/chart/renderer/xy/XYBarRendererTest.java
@@ -52,7 +52,6 @@ import org.jfree.chart.internal.CloneUtils;
 import org.jfree.chart.api.PublicCloneable;
 import org.jfree.data.Range;
 import org.jfree.data.xy.DefaultIntervalXYDataset;
-import org.jfree.data.xy.XYBarDataset;
 import org.jfree.data.xy.XYIntervalSeries;
 import org.jfree.data.xy.XYIntervalSeriesCollection;
 import org.jfree.data.xy.XYSeries;
@@ -321,42 +320,4 @@ public class XYBarRendererTest {
         assertEquals(10.0, r.getLowerBound(), EPSILON);
         assertEquals(20.0, r.getUpperBound(), EPSILON);
     }
-
-    /**
-     * A check for the datasetIndex and seriesIndex fields in the LegendItem
-     * returned by the getLegendItem() method.
-     */
-    @Test
-    public void testGetLegendItemSeriesIndex() {
-        XYSeriesCollection<String> d1 = new XYSeriesCollection<>();
-        XYSeries<String> s1 = new XYSeries<>("S1");
-        s1.add(1.0, 1.1);
-        XYSeries<String> s2 = new XYSeries<>("S2");
-        s2.add(1.0, 1.1);
-        d1.addSeries(s1);
-        d1.addSeries(s2);
-
-        XYSeriesCollection<String> d2 = new XYSeriesCollection<>();
-        XYSeries<String> s3 = new XYSeries<>("S3");
-        s3.add(1.0, 1.1);
-        XYSeries<String> s4 = new XYSeries<>("S4");
-        s4.add(1.0, 1.1);
-        XYSeries<String> s5 = new XYSeries<>("S5");
-        s5.add(1.0, 1.1);
-        d2.addSeries(s3);
-        d2.addSeries(s4);
-        d2.addSeries(s5);
-
-        XYBarRenderer r = new XYBarRenderer();
-        XYPlot<String> plot = new XYPlot<>(new XYBarDataset<>(d1, 1.0),
-                new NumberAxis("x"), new NumberAxis("y"), r);
-        plot.setDataset(1, new XYBarDataset<>(d2, 2.0));
-        JFreeChart chart = new JFreeChart(plot);
-        LegendItem li = r.getLegendItem(1, 2);
-        assertEquals("S5", li.getLabel());
-        assertEquals(1, li.getDatasetIndex());
-        assertEquals(2, li.getSeriesIndex());
-    }
-
-
 }
